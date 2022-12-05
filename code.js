408b41857ab64b223f9d34299077bde75e82be35
Xenia DeNoyer <90346767+Ainex8000@users.noreply.github.com>
2022-12-05 12:53:09 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+function goUATSpace(){
+    console.log("goUATSpace() started");
+    location.replace("login.html");
+}
+
+function boards(){
+    console.log("boards() started");
+    location.replace("boards.html");
+}
+
+function goIndex(){
+    console.log("goIndex() started");
+    location.replace("index.html");
+}
